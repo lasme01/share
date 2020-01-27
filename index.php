c5e5ebfa83f3ae7086e086d6d9f8259ca413d6e8
zimpact <yakpalasme@gmail.com>
2020-01-27 18:46:30 -0500

    first page for sharing file

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+  <meta charset="utf-8">
+  <title> Centre de Téléchargerment</title>
+</head>
+
+<body>
+  <section>
+      <h1> Centre de Téléchargement </h1> <br><br>
+	<p> "Welcome to the file sharing, <br/> This plateform is a very suitable interface which aims to helps you exchange your file easily </p>
+      <a href="InstallMacOSX.dmg"> Cliquez-ici pour télécharger MacOS</a>
+
+  </section>
+
+  <section>
+      <form>
+          <input type="file" name="telecarge">
+      </form>
+  </section>
+
+
+</body>
+
+</html>
